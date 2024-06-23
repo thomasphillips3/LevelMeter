f8abd01df48d55c9595073dc3ef633185c40ee33
Thomas Phillips <thomasphillips3@gmail.com>
2024-06-23 14:10:43 -0700

    get rms level

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -54,6 +54,6 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
-    //==============================================================================
+    float rmsLevelLeft, rmsLevelRight;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LevelMeterAudioProcessor)
 };
