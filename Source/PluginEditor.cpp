cb55f35933e9afa14c501fc153dcdf9657787e2f
Thomas Phillips <thomasphillips3@gmail.com>
2024-07-15 17:29:02 -0700

    add labels, values, slider and button

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,24 +1,28 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
 //==============================================================================
 LevelMeterAudioProcessorEditor::LevelMeterAudioProcessorEditor (LevelMeterAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p),
-    verticalGradientMeterL([&]() { return audioProcessor.getRmsValue(0); }),
-    verticalGradientMeterR([&]() { return audioProcessor.getRmsValue(1); }),
-    verticalDiscreteMeterL([&]() { return audioProcessor.getRmsValue(0); }),
-    verticalDiscreteMeterR([&]() { return audioProcessor.getRmsValue(1); }),
-    circularMeterL([&]() { return audioProcessor.getRmsValue(0); }, Colours::green),
-    circularMeterR([&]() { return audioProcessor.getRmsValue(1); }, Colours::cyan)
+    : AudioProcessorEditor(&p), audioProcessor(p),
+    leftSliderAttachment(p.getApvts(), "left", leftSlider),
+    rightSliderAttachment(p.getApvts(), "right", rightSlider),
+    rmsPeriodAttachment(p.getApvts(), "rmsPeriod", rmsPeriodSlider),
+    enableSmoothingAttachment(p.getApvts(), "smoothing", enableSmoothingButton),
+    verticalGradientMeterL([&]() { return audioProcessor.getRmsLevel(0); }),
+    verticalGradientMeterR([&]() { return audioProcessor.getRmsLevel(1); }),
+    verticalDiscreteMeterL([&]() { return audioProcessor.getRmsLevel(0); }),
+    verticalDiscreteMeterR([&]() { return audioProcessor.getRmsLevel(1); }),
+    circularMeterL([&]() { return audioProcessor.getRmsLevel(0); }, Colours::fuchsia),
+    circularMeterR([&]() { return audioProcessor.getRmsLevel(1); }, Colours::cyan)
 {
+    addAndMakeVisible(rmsLevelHeading1);
+    addAndMakeVisible(rmsLevelHeading2);
+    addAndMakeVisible(currentRmsLabel);
+    addAndMakeVisible(currentRmsValue);
+    addAndMakeVisible(maxRmsLabel);
+    addAndMakeVisible(maxRmsValue);
+    addAndMakeVisible(rmsPeriodLabel);
+    
     addAndMakeVisible(horizontalMeterL);
     addAndMakeVisible(horizontalMeterR);
     
@@ -31,7 +35,45 @@ LevelMeterAudioProcessorEditor::LevelMeterAudioProcessorEditor (LevelMeterAudioP
     addAndMakeVisible(circularMeterL);
     addAndMakeVisible(circularMeterR);
     
+    addAndMakeVisible(leftSlider);
+    addAndMakeVisible(rightSlider);
+    addAndMakeVisible(rmsPeriodSlider);
+    addAndMakeVisible(enableSmoothingButton);
+    
+    rmsLevelHeading1.setText("dBFS", dontSendNotification);
+    rmsLevelHeading1.setFont(Font{}.withStyle(Font::FontStyleFlags::bold));
+    
+    rmsLevelHeading2.setText("Left \t Right", dontSendNotification);
+    rmsLevelHeading2.setFont(Font{}.withStyle(Font::FontStyleFlags::bold));
+    
+    currentRmsLabel.setText("Current RMS:", dontSendNotification);
+    maxRmsLabel.setText("Max RMS:", dontSendNotification);
+    rmsPeriodLabel.setText("RMS Period", dontSendNotification);
+    rmsPeriodLabel.setJustificationType(Justification::right);
+    
+    leftSlider.setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
+    leftSlider.setTextBoxStyle(Slider::NoTextBox, true, 0, 0);
+    leftSlider.setPopupDisplayEnabled(true, false, this);
+    leftSlider.setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::violet.withBrightness(0.6f));
+    leftSlider.setColour(Slider::ColourIds::thumbColourId, Colours::violet);
+    leftSlider.setTextValueSuffix(" dB : Left Channel");
+    
+    rightSlider.setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
+    rightSlider.setTextBoxStyle(Slider::NoTextBox, true, 0, 0);
+    rightSlider.setPopupDisplayEnabled(true, false, this);
+    rightSlider.setColour(Slider::ColourIds::rotarySliderFillColourId, Colours::turquoise.withBrightness(0.6f));
+    rightSlider.setColour(Slider::ColourIds::thumbColourId, Colours::turquoise);
+    rightSlider.setTextValueSuffix(" dB : Right Channel");
+
+    rmsPeriodSlider.setSliderStyle(Slider::LinearHorizontal);
+    rmsPeriodSlider.setTextBoxStyle(Slider::NoTextBox, true, 0, 0);
+    rmsPeriodSlider.setPopupDisplayEnabled(true, false, this);
+    rmsPeriodSlider.setTextValueSuffix(" ms");
+    
+    enableSmoothingButton.setButtonText("Enable smoothing");
+    
     setSize (400, 500);
+    setResizable(true, true);
     startTimerHz(24);
 }
 
@@ -41,8 +83,8 @@ LevelMeterAudioProcessorEditor::~LevelMeterAudioProcessorEditor()
 
 void LevelMeterAudioProcessorEditor::timerCallback()
 {
-    horizontalMeterL.setLevel(audioProcessor.getRmsValue(0));
-    horizontalMeterR.setLevel(audioProcessor.getRmsValue(1));
+    horizontalMeterL.setLevel(audioProcessor.getRmsLevel(0));
+    horizontalMeterR.setLevel(audioProcessor.getRmsLevel(1));
     
     horizontalMeterL.repaint();
     horizontalMeterR.repaint();
@@ -55,6 +97,29 @@ void LevelMeterAudioProcessorEditor::paint (juce::Graphics& g)
 
 void LevelMeterAudioProcessorEditor::resized()
 {
+    const auto container = getBounds().reduced(20);
+    auto bounds = container;
+    
+    auto labelBounds = bounds.removeFromTop(container.proportionOfHeight(0.12f));
+    auto controlBounds = labelBounds.removeFromRight(container.proportionOfWidth(0.35f));
+    
+    const auto labelHeight = labelBounds.proportionOfHeight(0.33f);
+    auto labelRow1 = labelBounds.removeFromTop(labelHeight);
+    rmsLevelHeading1.setBounds(labelRow1.removeFromLeft(labelRow1.proportionOfWidth(0.5f)));
+    rmsLevelHeading2.setBounds(labelRow1);
+    
+    auto labelRow2 = labelBounds.removeFromTop(labelHeight);
+    maxRmsLabel.setBounds(labelRow2.removeFromLeft(labelRow2.proportionOfWidth(0.5f)));
+    maxRmsLabel.setBounds(labelRow2);
+    
+    auto labelRow3 = labelBounds;
+    currentRmsLabel.setBounds(labelRow3.removeFromLeft(labelRow3.proportionOfWidth(0.5f)));
+    currentRmsLabel.setBounds(labelRow3);
+    
+    rmsPeriodLabel.setBounds(controlBounds.removeFromTop(labelHeight));
+    rmsPeriodSlider.setBounds(controlBounds.removeFromTop(labelHeight));
+    enableSmoothingButton.setBounds(controlBounds);
+    
     horizontalMeterL.setBounds(100, 100, 200, 15);
     horizontalMeterR.setBounds(100, 120, 200, 15);
     
